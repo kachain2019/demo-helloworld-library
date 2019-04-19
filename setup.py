4ceb7d9f5ac05dcedcda2d49cbf2d30a1720a84f
kachain.a <kachain.a@entronica.co.th>
2019-04-19 11:55:10 +0700

    Hello First Library

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="helloworld-library",
+    name="chain-helloworld-library",
     version='1.0',
     package_dir={'' : 'src'},
     packages=['HelloWorld'],
-    url='https://github.com/up1/demo-helloworld-library',
-    author='Somkiat',
-    author_email='somkiat.p@gmail.com',
+    url='https://github.com/kachain2019/demo-helloworld-library',
+    author='kachain',
+    author_email='kachain2019.a@gmail.com',
 )
