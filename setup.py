d9042d56e58aca65a75b9caacaa00c60a3162242
kachain.a <kachain.a@entronica.co.th>
2019-04-29 10:54:53 +0700

    Hello

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="chain-helloworld-library",
+    name="chain2-helloworld-library",
     version='1.0',
     package_dir={'' : 'src'},
     packages=['HelloWorld'],
